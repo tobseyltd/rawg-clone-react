d84f417d4572512a81923e5cbdcf2d8d2470c7da
tobseyltd <mail@tobsey.limited>
2023-05-05 14:40:25 +0200

    created SearchBar Component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch";
 import Logo from "../assets/Logo/logo.webp";
+import SearchBar from "./SearchBar";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent={"space-between"} alignItems={"center"}>
-      <Image src={Logo} boxSize={50}></Image>
+    <HStack spacing={6} alignItems={"center"}>
+      <Image src={Logo} boxSize={59}></Image>
+      <SearchBar />
       <ColorModeSwitch />
     </HStack>
   );
