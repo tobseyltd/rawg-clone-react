7edb4e41de447d9dad4f470a4bfd888f4846e091
tobseyltd <mail@tobsey.limited>
2023-05-05 16:43:21 +0200

    ship genres with the app as hard coded data

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,4 @@
-import useData from "./useData";
+import Genres from "../data/Genres";
 
 export interface Genres {
     id: number;
@@ -6,6 +6,6 @@ export interface Genres {
     image_background: string;
 }
 
-const useGenres = () => useData<Genres>('/genres')
+const useGenres = () => ({ data: Genres, isLoading: false, error: null })
 
 export default useGenres
