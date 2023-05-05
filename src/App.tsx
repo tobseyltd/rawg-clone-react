a1cbdc2984d9f839e59bf098169efdb9d91ad60c
tobseyltd <mail@tobsey.limited>
2023-05-05 08:53:30 +0200

    implemented ux by adding highlights to selected genre

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => {
       </GridItem>
       <Show above='lg'>
         <GridItem area={"aside"}>
-          <GenreList onSelectedGenre={(genre) => setSelectedGenre(genre)} />
+          <GenreList selectedGenre={selectedGenre} onSelectedGenre={(genre) => setSelectedGenre(genre)} />
         </GridItem>
       </Show>
       <GridItem area={"main"}>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,22 +1,13 @@
-import {
-  Button,
-  HStack,
-  Image,
-  Link,
-  List,
-  ListItem,
-  Spinner,
-  Text,
-  textDecoration,
-} from "@chakra-ui/react";
+import { HStack, Image, Link, List, ListItem, Spinner } from "@chakra-ui/react";
 import useGenres, { Genres } from "../hooks/useGenres";
 import getCroppedImages from "../services/CroppedGameCardImages";
 
 interface Props {
   onSelectedGenre: (genre: Genres) => void;
+  selectedGenre: Genres | null;
 }
 
-const GenreList = ({ onSelectedGenre }: Props) => {
+const GenreList = ({ onSelectedGenre, selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -31,7 +22,14 @@ const GenreList = ({ onSelectedGenre }: Props) => {
               borderRadius={10}
               src={getCroppedImages(genre.image_background)}
             />
-            <Link className='genrelink' fontSize={17}>
+            <Link
+              textDecoration={
+                genre.id === selectedGenre?.id ? "underline" : "normal"
+              }
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+              className='genrelink'
+              fontSize={17}
+            >
               {genre.name}
             </Link>
           </HStack>
